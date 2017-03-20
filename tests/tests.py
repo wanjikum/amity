c83e28d4ab3efba4d70485ce00bfba6c2aaa6812
wanjikum <wanjikumillicent5@gmail.com>
2017-03-20 23:01:44 +0300

    create failing test on inheritance on class person

--- tests/tests.py
+++ tests/tests.py
@@ -12,12 +12,30 @@ class CreateRoomTestCases(unittest.TestCase):
         self.living_space = LivingSpace()
 
     def tearDown(self):
-        """"""
         del self.room
         del self.office
         del self.living_space
 
-    def test_office_and_living_space_is_instance_of_room(self):
+    def test_if_office_and_living_space_is_instance_of_room(self):
         """Tests if office and living space are instances of class Room"""
+        self.assertTrue(isinstance(self.office, Room))
+        self.assertTrue(isinstance(self.living_space, Room))
+
+
+class AddPersonTestCases(unittest.TestCase):
+    """A class CreateRoom that has a collection of create_room testcases"""
+    def setUp(self):
+        """Keeps our code dry"""
+        self.person = Person()
+        self.fellow = Fellow()
+        self.staff = Staff()
+
+    def tearDown(self):
+        del self.person
+        del self.fellow
+        del self.staff
+
+    def test_if_fellow_and_staff_is_instance_of_room(self):
+        """Tests if fellow and staff are instances of class Person"""
         self.assertTrue(isinstance(self.office, Room))
         self.assertTrue(isinstance(self.living_space, Room))
