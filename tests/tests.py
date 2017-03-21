557c4bc48affb6ec9c6d694daf21dc44b20789cf
wanjikum <wanjikumillicent5@gmail.com>
2017-03-21 08:28:41 +0300

    Create more tests of class add person

--- tests/tests.py
+++ tests/tests.py
@@ -17,7 +17,7 @@ class CreateRoomTestCases(unittest.TestCase):
         del self.office
         del self.living_space
 
-    def test_if_office_and_living_space_is_instance_of_room(self):
+    def test_if_office_and_living_space_is_instance_of_class_room(self):
         """Tests if office and living space are instances of class Room"""
         self.assertTrue(isinstance(self.office, Room))
         self.assertTrue(isinstance(self.living_space, Room))
@@ -36,7 +36,17 @@ class AddPersonTestCases(unittest.TestCase):
         del self.fellow
         del self.staff
 
-    def test_if_fellow_and_staff_is_instance_of_room(self):
+    def test_if_fellow_and_staff_is_instance_of_class_person(self):
         """Tests if fellow and staff are instances of class Person"""
         self.assertTrue(isinstance(self.fellow, Person))
         self.assertTrue(isinstance(self.staff, Person))
+
+    def test_if_the_class_fellow_takes_in_correct_attributes(self):
+        """Tests if class fellow takes in the attributes given"""
+        self.assertListEquals(["Jamhuri Linnet", "yes"],
+                              [self.fellow.person_name,
+                               self.fellow.accommodate])
+
+    def test_class_staff_takes_in_correct_attributes(self):
+        """Tests if class staff takes in the attributes given"""
+        self.assertEqual("Oliver", self.staff.person_name)
